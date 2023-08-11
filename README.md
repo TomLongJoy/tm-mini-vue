53148aa672b14b5812544bc2abde682476b7aa71
TomLongJoy <471727371@qq.com>
2023-08-11 16:06:08 +0800

    effect和reactive逻辑提交
    创建effect.ts和reactive.ts文件，创建逻辑。

--- README.md
+++ README.md
@@ -4,7 +4,6 @@
 # 1创建步骤 vue3 pinia ts  node v16.20.0  
 
 ```js 
-     
 
     1. yarn init -y 
 
@@ -18,11 +17,39 @@
 
     6. package.json 文件，添加scripts字段 {"test":"jest"}
 
-    7. yarn test 测试运行  
+    7. yarn test 测试运行      
 
-    
-     
+    8. yarn add --dev babel-jest @babel/core @babel/preset-env
 
+    9. yarn add --dev @babel/preset-typescript 
+
+    10. 
+    babel.config.js文件内容
+    module.exports = {
+        presets: [
+            ['@babel/preset-env', { targets: { node: 'current' } }],
+            '@babel/preset-typescript',
+        ],
+    };
+
+    11. yarn test 
+
+    12. 查看目录结构
+    tree -aI ".git*|.vscode|node_modules" -C -L 4
+    .
+    ├── LICENSE
+    ├── README.md
+    ├── babel.config.js
+    ├── package.json
+    ├── src
+    │   └── reactivity
+    │       ├── index.ts
+    │       └── tests
+    │           └── index.spec.ts
+    ├── tsconfig.json
+    └── yarn.lock
+
+    3 directories, 8 files
 ```
 
 # ps: 模块关系图
---
+++ src/reactivity/effect.ts
@@ -0,0 +1,55 @@
+
+
+class ReactiveEffect {
+    private _fn: any;
+
+    constructor(fn: any) {
+
+        this._fn = fn;
+    }
+    run() {
+        activeEffect = this;
+        this._fn();
+    }
+}
+
+
+
+const targetMap = new Map();
+export function track(target: any, key: any) {
+
+    // target -> key -> dep
+    let depsMap = targetMap.get(target);
+
+    if (!depsMap) {
+        depsMap = new Map();
+        targetMap.set(target, depsMap)
+    }
+    let dep = depsMap.get(key);
+    if (!dep) {
+        dep = new Set();
+        depsMap.set(key, dep)
+    }
+    // const dep = new Set();
+
+    dep.add(activeEffect)
+
+}
+
+export function trigger(target: any, key: string | symbol) {
+
+    let depsMap = targetMap.get(target);
+    let dep = depsMap.get(key);
+    for (const effect of dep) {
+        effect.run(); 
+    }
+}
+
+let activeEffect: any;
+export function effect(fn: any) {
+    // fn 
+    const _effect = new ReactiveEffect(fn)
+
+
+    _effect.run();
+}
