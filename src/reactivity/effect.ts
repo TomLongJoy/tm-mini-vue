d5a18221a3ef90c671420d3d560cbf0a6dced800
TomLongJoy <471727371@qq.com>
2023-08-15 11:46:47 +0800

    onStop功能
    添加onStop功能。

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,14 +1,35 @@
+import { extend } from "../shared";
+
 class ReactiveEffect {
     private _fn: any;
-    constructor(fn: any, public scheduler?: any) {
+    deps = [];
+    active = true;
+    onStop?: () => void;
+    public scheduler: Function | undefined;
+    constructor(fn: any, public _scheduler?: Function) {
         this._fn = fn;
+        this.scheduler = _scheduler;
     }
     run() {
         activeEffect = this;
         return this._fn();
     }
+    stop() {
+        if (this.active) {
+            cleanupEffect(this);
+            if (this.onStop) {
+                this.onStop();
+            }
+            this.active = false;
+        }
+    }
 }
 
+function cleanupEffect(effect: any) {
+    effect.deps.forEach((dep: any) => {
+        dep.delete(effect)
+    });
+}
 const targetMap = new Map();
 export function track(target: any, key: any) {
     // target -> key -> dep
@@ -22,9 +43,11 @@ export function track(target: any, key: any) {
         dep = new Set();
         depsMap.set(key, dep)
     }
+    if(!activeEffect) return;
     dep.add(activeEffect)
-    console.log("--------添加完成");
+    activeEffect.deps.push(dep);
 }
+
 export function trigger(target: any, key: string | symbol) {
     let depsMap = targetMap.get(target);
     let dep = depsMap.get(key);
@@ -39,7 +62,22 @@ export function trigger(target: any, key: string | symbol) {
 let activeEffect: any;
 export function effect(fn: any, options: any = {}) {
     //fn
-    const _effect = new ReactiveEffect(fn, options.scheduler)
+    const _effect = new ReactiveEffect(fn, options.scheduler);
+
+    // _effect.onStop = options.onStop;
+    //options 
+    // Object.assign(_effect,options)
+    extend(_effect, options)
+
     _effect.run();
-    return _effect.run.bind(_effect);//bind -- learn 2 
+    const runner: any = _effect.run.bind(_effect);
+    /*
+        runner是个函数，怎么能 .effect
+        https://juejin.cn/post/7076967942079905806 
+    */
+    runner.effect = _effect; // 这里忘记写，报错了。 
+    return runner;//bind -- learn 2 
+}
+export function stop(runner: any) {
+    runner.effect.stop();
 }
