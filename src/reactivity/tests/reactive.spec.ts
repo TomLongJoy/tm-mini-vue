c08949c056d2444665c4d9cdfc07cfbfe9d8b36b
TomLongJoy <471727371@qq.com>
2023-11-18 15:33:41 +0800

    使用idea开发vue3
    整理

--- src/reactivity/tests/reactive.spec.ts
+++ src/reactivity/tests/reactive.spec.ts
@@ -1,10 +1,17 @@
 import { reactive } from "../reactive";
 describe("reactive", () => {
-    it("happy path ", () => {
-        const original = { foo: 1 };
-        const observed = reactive(original);
-        expect(observed).not.toBe(original);
-        expect(observed.foo).toBe(1);
-    })
-    
+      it("happy path ", () => {
+            const original = { foo: 1 };
+
+            /*
+            Reflect 
+            Proxy
+
+            */
+            const observed = reactive(original);
+            // const observedTwo = original;
+            expect(observed).not.toBe(original);
+            expect(observed.foo).toBe(1);
+      })
+
 })
